4facd9fff83267e230ee4ad6dcc249f945107bdc
Siigmaf <faby.tbe@gmail.com>
2022-11-09 11:55:35 -0300

    starting project

---
+++ calculator/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1><strong>Calculator</strong></h1>
+      </header>
+      <section className="calculator-field">
+          <h2>CALCULATOR</h2>
+        </section>
+      <footer className="App-footer">
+        Calculator Project - Developed by Fabiana Ignácio - 2022
+        </footer>
+    </div>
+  );
+}
+
+export default App;
