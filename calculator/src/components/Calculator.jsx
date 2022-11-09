f0c5d685e243f89761d8eb03e6cdca7d53a67a65
Siigmaf <faby.tbe@gmail.com>
2022-11-09 13:41:59 -0300

    add arithmetic operations buttons

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -5,15 +5,26 @@ export default function Calcultor() {
     return (
         <section className="calculator">
             <div className="container-digits">
-            <button type="button" className="digits">1</button>
-            <button type="button" className="digits">2</button>
-            <button type="button" className="digits">3</button>
-            <button type="button" className="digits">4</button>
-            <button type="button" className="digits">5</button>
-            <button type="button" className="digits">6</button>
+            <button type="button" className="digits">AC</button>
             <button type="button" className="digits">7</button>
+            <button type="button" className="digits">4</button>
+            <button type="button" className="digits">1</button>
+            <button type="button" className="digits">0</button>
+            <button type="button" className="digits">( )</button>
             <button type="button" className="digits">8</button>
+            <button type="button" className="digits">5</button>
+            <button type="button" className="digits">2</button>
+            <button type="button" className="digits">.</button>
+            <button type="button" className="digits">%</button>
             <button type="button" className="digits">9</button>
+            <button type="button" className="digits">6</button>
+            <button type="button" className="digits">3</button>
+            <button type="button" className="digits">&lArr;</button>
+            <button type="button" className="digits">/</button>
+            <button type="button" className="digits">*</button>
+            <button type="button" className="digits">-</button>
+            <button type="button" className="digits">+</button>
+            <button type="button" className="digits">=</button>
             </div>
         </section>
     )
