91386814cfe220a655df963d0659f03bd5bce3e5
Siigmaf <faby.tbe@gmail.com>
2022-11-09 12:50:26 -0300

    creating the digits buttons

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import './calculator.css'
 
-export default function Calcultor (){
-    return(
-        <p>iniciando</p>
+export default function Calcultor() {
+    return (
+        <section className="calculator">
+            <div className="container-digits">
+            <button type="button" className="digits">1</button>
+            <button type="button" className="digits">2</button>
+            <button type="button" className="digits">3</button>
+            <button type="button" className="digits">4</button>
+            <button type="button" className="digits">5</button>
+            <button type="button" className="digits">6</button>
+            <button type="button" className="digits">7</button>
+            <button type="button" className="digits">8</button>
+            <button type="button" className="digits">9</button>
+            </div>
+        </section>
     )
 }
