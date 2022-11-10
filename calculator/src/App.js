e3ba9d568a0040eb32b0c7c4062615717824d5e4
Siigmaf <faby.tbe@gmail.com>
2022-11-10 14:03:09 -0300

    add effect to buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
+import { FcCalculator } from 'react-icons/fc';
 import Calcultor from './components/Calculator';
 
 function App() {
+  const DATE = new Date();
+  const DAY = String(DATE.getDate());
+  const MONTH = String(DATE.getMonth() + 1);
+  const YEAR = DATE.getFullYear();
+  const currentDate = `${DAY}/${MONTH}/${YEAR}`
+
+  const HOURS = DATE.getHours();
+  const MIN = DATE.getMinutes();
+  const SEC = DATE.getSeconds();
+  const currentHours = `${HOURS}/${MIN}/${SEC}`
+
+  console.log(currentDate);
   return (
     <div className="App">
       <header className="App-header">
+        <FcCalculator style={{ fontSize: '50px', color: ' #FFD700' }} />
         <h1><strong>Calculator</strong></h1>
+        <h5>{currentDate}</h5>
+        <h5>{currentHours}</h5>
       </header>
       <section className="calculator-field">
-          <Calcultor />
-        </section>
+        <Calcultor />
+      </section>
       <footer className="App-footer">
         Calculator Project - Developed by Fabiana Ignácio - 2022
-        </footer>
+      </footer>
     </div>
   );
 }
--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -4,27 +4,30 @@ import './calculator.css'
 export default function Calcultor() {
     return (
         <section className="calculator">
+            <div className="result-screen">
+                <h1 className="result">0</h1>
+            </div>
             <div className="container-digits">
-            <button type="button" className="digits">AC</button>
+            <button type="button" className="grey">AC</button>
             <button type="button" className="digits">7</button>
             <button type="button" className="digits">4</button>
             <button type="button" className="digits">1</button>
             <button type="button" className="digits">0</button>
-            <button type="button" className="digits">( )</button>
+            <button type="button" className="grey">( )</button>
             <button type="button" className="digits">8</button>
             <button type="button" className="digits">5</button>
             <button type="button" className="digits">2</button>
             <button type="button" className="digits">.</button>
-            <button type="button" className="digits">%</button>
+            <button type="button" className="grey">%</button>
             <button type="button" className="digits">9</button>
             <button type="button" className="digits">6</button>
             <button type="button" className="digits">3</button>
             <button type="button" className="digits">&lArr;</button>
-            <button type="button" className="digits">/</button>
-            <button type="button" className="digits">*</button>
-            <button type="button" className="digits">-</button>
-            <button type="button" className="digits">+</button>
-            <button type="button" className="digits">=</button>
+            <button type="button" className="orange">/</button>
+            <button type="button" className="orange">*</button>
+            <button type="button" className="orange">-</button>
+            <button type="button" className="orange">+</button>
+            <button type="button" className="orange">=</button>
             </div>
         </section>
     )
